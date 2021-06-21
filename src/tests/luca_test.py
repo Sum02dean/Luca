23a0f76b6847908f9b15539f175954e8c9044e39
Dean Sumner <36505689+Sum02dean@users.noreply.github.com>
2021-06-21 13:24:07 +0200

    Update luca_test.py

--- src/tests/luca_test.py
+++ src/tests/luca_test.py
@@ -12,8 +12,11 @@ def test_method():
     # Grab data
     x = pd.read_csv('../../data/20210103_hundenamen.csv')
     dog_names = x.HUNDENAME.values
-
-    # Check all scores evaluate to exactly 1
-    r = L.compute_distance(dog_names)
+    
+    
+    # Check all scores evaluate to score_filter
+    score_filter= 1
+    r = L.compute_distance(dog_names, score_filter)
+    
     assert [x for x in L.test_scores] == [
-        1 for x in range(0, len(L.test_scores))]
+        score_filter for x in range(0, len(L.test_scores))]
